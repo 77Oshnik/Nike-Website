69dd27b3328344f26409ec5bd498aba7caa6c63d
77Oshnik <tiwarioshnikdeep@gmail.com>
2024-06-30 12:10:12 +0530

    Practice project in react

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+function Hero() {
+  return (
+    <main>
+      <div className="text">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+          eveniet officiis velit aperiam neque repudiandae tenetur cum esse
+          quasi facilis.
+        </p>
+        <div className="button">
+            <button>shop now</button>
+            <button>category</button>
+        </div>
+        <div className="shopping">
+            <p>Lorem ipsum dolor sit.</p>
+        </div>
+        <div className="icon">
+            <img src="/images/amazon.png" alt="" />
+            <img src="/images/flipkart.png" alt="" />
+        </div>
+
+      </div>
+      <div className="image">
+        <img src="/images/shoe_image.png" alt="" />
+      </div>
+    </main>
+  );
+}
+export default Hero;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+function Navigation(){
+    return(
+        <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" srcset="" />
+        </div>
+
+        <ul>
+          <li>MENU</li>
+          <li>LOCATION</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    )
+}
+export default Navigation
